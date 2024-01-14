ca76f079b81951195522692837e8981f3f655f38
Taimoor2500 <taimoorrashid858@gmail.com>
2024-01-15 00:42:48 +0500

    middlewares

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const app = express()
 
 const PORT = process.env.PORT || 5000;
 
-
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.json())
 //routes
 app.get("/",(req,res)=> {
     res.send("Home Page");
